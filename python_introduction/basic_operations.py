b666860debaa661dd9082905f9eacbb907a3226e
MSEgbi-Kumah <sek,mek13@gmail.com>
2025-03-20 17:56:47 +0000

    My First commit message of this repo

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+Number1 = 10
+Number2 = 5
+int(Number1) + int(Number2) 
+Addition of Number1 and Number2 is 15
+int(Number1) - int(Number2)
+Subtraction of Number1 and Number2 is 5
+int(Number1) X int(Number2)
+Multiplication of Number1 and Number2 50

0867c6bb87b391de038fb45501ae3eb0e567358b
MSEgbi-Kumah <sek,mek13@gmail.com>
2025-03-20 18:53:43 +0000

    My First commit message of this repo

--- basic_operations.py
+++ basic_operations.py
@@ -1,7 +1,7 @@
-Number1 = 10
-Number2= 5
-Number1 = int(Number1)
-Number2 = int(Number2)
-Print(Addition of Number1 + Number2 is 15)
-Print(Subtraction of Number1 - Number2 is 5)
-Print(Multiplication of Number1 * Number2 is 50)
+number1 = 10
+number2= 5
+number1 = int(number1)
+number2 = int(number2)
+Print(Addition of number1 + number2 is 15)
+Print(Subtraction of number1 - number2 is 5)
+Print(Multiplication of number1 * number2 is 50)

c7e9efee79ac3b346112b715a5bcdda73d3cf124
MSEgbi-Kumah <sek,mek13@gmail.com>
2025-03-24 18:24:38 +0000

    Simple interest Calculation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,13 +1,13 @@
 # Variables for Simple Interst Formula
-P = 1000
-R = 0.05
-T = 3
+principal = 1000
+rate = 0.05
+time = 3
 
 # Making Sure Variables are intergers
-I = int(P * R * T)
-P = int(1000.0)
-R = int(0.050)
-T = int(3.0)
+interest = int(principal * rate * time)
+principal = int(1000.0)
+rate = int(0.050)
+time = int(3.0)
 
 #Performing Calculations and printing their results
-print('The simple interest is:', I)
+print('The simple interest is:', interest)

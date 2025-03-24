89e49bc2c2336699419d53de2cacc40139060794
MSEgbi-Kumah <sek,mek13@gmail.com>
2025-03-24 17:02:04 +0000

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,12 @@
-Number1 = 10
-Number2 = 5
-int(Number1) + int(Number2) 
-Addition of Number1 and Number2 is 15
-int(Number1) - int(Number2)
-Subtraction of Number1 and Number2 is 5
-int(Number1) X int(Number2)
-Multiplication of Number1 and Number2 50
+# Define variables
+number1 = 10
+number2 = 5
+
+# Ensure variables are integers (though unnecessary here since they're already integers)
+number1 = int(number1)
+number2 = int(number2)
+
+# Perform calculations and print results
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
